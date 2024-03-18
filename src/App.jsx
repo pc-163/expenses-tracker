c35efdd5ce38b674639b3d25443d0ff1449dbca8
pc-163 <rajputpc163@gmail.com>
2024-03-18 20:28:48 +0530

    working on expense tracker using vite and react

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+//import { useState } from 'react'
+import './App.css'
+import ExpenseForm from './components/expenseform'
+
+function App() {
+
+  return (
+    <>
+     <ExpenseForm />
+    </>
+  )
+}
+
+export default App
